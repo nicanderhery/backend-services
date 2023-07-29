b7bfc245f955faa4c4511b67079126b8b3c532ea
Agustinus Nicander Hery <nicanderhery@gmail.com>
2023-07-29 22:21:53 +0200

    Redirect root route to the latest version and add API v1 route
    - Redirect root route to '/v1' route
    - Add new '/v1' route that returns a 200 status with API v1 message

--- src/template/controller.ts
+++ src/template/controller.ts
@@ -2,8 +2,13 @@ import { Router } from 'express';
 
 const router = Router({ mergeParams: true });
 
+// Redirect to the latest version
 router.get('/', (_req, res) => {
-    res.send('Hello, World! from template');
+    res.redirect(_req.baseUrl + '/v1');
+});
+
+router.get('/v1', (_req, res) => {
+    res.status(200).send({ message: 'API v1' });
 });
 
 export default router;
