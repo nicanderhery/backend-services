65c21b499e19e963276ea109c534a439e840a5ba
Agustinus Nicander Hery <nicanderhery@gmail.com>
2023-08-03 22:05:36 +0000

    remove deprecated body parser

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
@@ -19,7 +18,7 @@ SM.set('apiRoute', apiRoute);
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 // Get all folder's names in the src folder
 const folders = fs.readdirSync(__dirname + '/');
